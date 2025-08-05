c8880d4dd7961301d74ed316e05ec69eb4dacd41
Helio Giesel <heliogiesel@gmail.com>
2025-08-05 22:35:57 +0100

    Environment update
    - Added prettier
    - Apply prettier
    - Added proxyconfig
    - Changed app.config to return ApplicationConfig
    - Adapt app.config logic in main.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,17 @@ import { provideRouter, Routes } from '@angular/router';
 import { ObjectiveContainerComponent } from './objective-container/objective-container.component';
 import { LoginPageComponent } from './authentication/login-page/login-page.component';
 import { CreateAccountPageComponent } from './authentication/create-account-page/create-account-page.component';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
   { path: 'create-account', component: CreateAccountPageComponent },
   { path: 'home', redirectTo: '/objectives', pathMatch: 'full' },
-  { path: 'objectives/:id', component: ObjectiveContainerComponent },
+  { path: 'objectives', component: ObjectiveContainerComponent },
 ];
 
-export const appConfig = [provideRouter(routes)];
+export const appConfig: ApplicationConfig = {
+  providers: [provideHttpClient(), provideRouter(routes)],
+};
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [...appConfig],
-}).catch((e) => console.error(e));
+bootstrapApplication(AppComponent, appConfig).catch((e) => console.error(e));
