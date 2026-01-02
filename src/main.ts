f5d8300cc5f03efda193749eddcfabf360884d47
Helio Giesel <heliogiesel@gmail.com>
2026-01-02 09:51:47 +0000

    chore: Updated angular dependencies to 21

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch((e) => console.error(e));
+bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]}).catch((e) => console.error(e));
